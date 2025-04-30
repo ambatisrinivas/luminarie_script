16aceb9726c0437e1d578610c47e4c48fd872de4
Nishanth Ambati <133779317+NishanthAmabati@users.noreply.github.com>
2025-05-01 04:19:58 +0530

    Add files via upload
    Initial commit: Implemented Luminaire Control System
    
    - Added frontend (React) with luminaire control UI, charts for CCT and intensity, and WebSocket integration
    - Implemented backend (Python) with WebSocket server, luminaire operations, and scene scheduling
    - Included logging with timestamped log files and enhanced debugging

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y python3 python3-pip build-essential libssl-dev libffi-dev nodejs
+sudo pip3 install websockets psutil --break-system-packages
+sudo npm install react react-dom react-chartjs-2 chart.js chartjs-plugin-annotation react-icons
+sudo npm install -g serve 
