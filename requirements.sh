6b9c4ee95f66a13619e719f81d177482e2dd9e0c
Ambati Srinivasulu <ambati.srinivas@gmail.com>
2025-05-03 13:37:30 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,8 @@
 sudo apt update
-sudo apt install -y python3 python3-pip build-essential libssl-dev libffi-dev nodejs
+sudo apt install -y python3 python3-pip build-essential libssl-dev libffi-dev
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install nodejs -y
 pip3 install websockets psutil --break-system-packages
 sudo npm install react react-dom react-chartjs-2 chart.js chartjs-plugin-annotation react-icons
 sudo npm install -g serve 
+mv controlAPI.py ../.controlAPI.py
